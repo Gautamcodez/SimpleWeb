515f524eb2cd80addadcab4e17af817963a5134b
Gautam Sharma <Gautamcodez@gmail.com>
2022-08-05 19:37:07 +0530

    made final changes to form validation including saving the form data

--- Form-validator/script.js
+++ Form-validator/script.js
@@ -1,26 +1,73 @@
 const form = document.getElementById('form');
 const password1El = document.getElementById('password1');
 const password2El = document.getElementById('password2');
-const messageContainer = document.querySelector('message-container');
+const messageContainer = document.querySelector('.message-container');
 const message = document.getElementById('message');
 
 let isValid = false;
+let passwordMatch = false;
 
-function validateForm(){
+function validateForm() {
     // Using constraint Api
     isValid = form.checkValidity();
     // console.log(isValid);
-    message.textContent = 'Please fill out all the fields';
-    message.style.color = 'red';
-    messageContainer.style.borderColor = 'red';
 
+    // Styling main message for an error
+    if (!isValid) {
+        message.textContent = 'Make sure passwords match.';
+        message.style.color = 'red';
+        messageContainer.style.borderColor = 'red';
+        return;
+    }
+
+    // Check to see if passwords match
+    if (password1El.value == password2El.value) {
+        // If they match, set value to true and borders to green
+        passwordMatch = true;
+        password1El.style.borderColor = 'green';
+        password2El.style.borderColor = 'green';
+
+    } else {
+        passwordMatch = false;
+        // If they don't match, border color of input to red, change message
+        message.textContent = 'Make sure passwords match';
+        message.style.color = 'red';
+        messageContainer.style.borderColor = 'red';
+        password1El.style.borderColor = 'red';
+        password2El.style.borderColor = 'red';
+        return;
+    }
+
+    // If form is valid and passwords match
+    if (isValid && passwordMatch) {
+        // Style main message for success
+        message.textContent = 'Successfully Registered!';
+        message.style.color = 'green';
+        messageContainer.style.borderColor = 'green';
+    }
 }
 
-function processFormData(e){
+// Do something with user data
+function storeFormData(){
+    const user = {
+        name: form.name.value,
+        phone: form.phone.value,
+        email: form.email.value,
+        website: form.website.value,
+        password: form.password.value
+    };
+    console.log(user);
+}
+
+function processFormData(e) {
     e.preventDefault();
     // console.log(e);
     // Validate Form
     validateForm();
+    // SUbmit form data is valid
+    if (isValid && passwordMatch){
+        storeFormData();
+    }
 }
 
 // Event Listner
