d09154781810ed624cbb8dbb29a0cae3e8a791f0
Gautam Sharma <Gautamcodez@gmail.com>
2022-08-17 23:34:01 +0530

    add items to column

--- Drag-and-drop/script.js
+++ Drag-and-drop/script.js
@@ -121,6 +121,29 @@ function dragEnter(column) {
   currentColumn = column;
 }
 
+// Add to column list, reset textbox
+function addToColumn(column){
+  const itemText = addItems[column].textContent;
+  const selectedArray = listArrays[column];
+  selectedArray.push(itemText);
+  updateDOM();
+}
+
+// Show Add Item Input Box
+function showInputBox(column){
+  addBtns[column].style.visibility = 'hidden';
+  saveItemBtns[column].style.display = 'flex';
+  addItemContainers[column].style.display = 'flex';
+}
+
+// Hide input box
+function hideInputBox(column){
+  addBtns[column].style.visibility = 'visible';
+  saveItemBtns[column].style.display = 'none';
+  addItemContainers[column].style.display = 'none';
+  addToColumn(column);
+}
+
 // Allow arrays to reflect drag and drp items
 function rebuildArrays(){
   // console.log(backlogList.children);
