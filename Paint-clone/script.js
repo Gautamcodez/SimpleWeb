97d7c7ff968ebe348967d93b358d3fa9ed9b0989
Gautam Sharma <Gautamcodez@gmail.com>
2022-08-20 20:57:13 +0530

    setup the canvas

--- Paint-clone/script.js
+++ Paint-clone/script.js
@@ -14,6 +14,9 @@ const { body } = document;
 
 // Global Variables
 
+const canvas = document.createElement('canvas');
+canvas.id = 'canvas';
+const context = canvas.getContext('2d');
 let currentSize = 10;
 let bucketColor = '#FFFFFF';
 let currentColor = '#A51DAB';
@@ -63,10 +66,10 @@ let currentColor = '#A51DAB';
 
 // Create Canvas
 function createCanvas() {
-  // canvas.width = ;
-  // canvas.height = ;
-  // context.fillStyle = ;
-  // context.fillRect();
+  canvas.width = window.innerWidth ;
+  canvas.height = window.innerHeight-50;
+  context.fillStyle = bucketColor ;
+  context.fillRect(0,0,canvas.width, canvas.height);
   body.appendChild(canvas);
 
 }
