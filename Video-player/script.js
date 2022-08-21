5d70f9e51218aab6bb01c29f46444baa648305e2
Gautam Sharma <Gautamcodez@gmail.com>
2022-08-21 17:16:33 +0530

    Progress Bar (Seek) functionality

--- Video-player/script.js
+++ Video-player/script.js
@@ -51,9 +51,12 @@ function updateProgress(){
 }
 
 // Click to seek within the video
-// function setProgress(e){
-
-// }
+function setProgress(e){
+    const newTime = e.offsetX / progressRange.offsetWidth;
+    progressBar.style.width = `${newTime * 100}%`;
+    video.currentTime = newTime * video.duration;
+    console.log(newTime);
+}
 
 // Volume Controls --------------------------- //
 
@@ -72,4 +75,4 @@ playBtn.addEventListener('click', togglePlay);
 video.addEventListener('click', togglePlay);
 video.addEventListener('timeupdate', updateProgress);
 video.addEventListener('canplay', updateProgress);
-// progressRange.addEventListener('click', setProgress);
+progressRange.addEventListener('click', setProgress);
