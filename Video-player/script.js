273886ce2b35eb198364772a3bb9b086ee9f06ce
Gautam Sharma <Gautamcodez@gmail.com>
2022-08-21 17:13:01 +0530

    populate current time/ duration

--- Video-player/script.js
+++ Video-player/script.js
@@ -34,10 +34,26 @@ video.addEventListener('ended', showPlayIcon);
 
 // Progress Bar ---------------------------------- //
 
+// Calculate display time format
+function displayTime(time) {
+    const minutes = Math.floor(time/60);
+    let seconds = Math.floor(time % 60);
+    seconds = seconds > 9 ? seconds : `0${seconds}`;
+    return `${minutes}:${seconds}`;
+    // console.log(minutes, seconds);
+}
+
 // Update progress bar as video plays
 function updateProgress(){
     progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`;
+    currentTime.textContent = `${displayTime(video.currentTime)}/`
+    duration.textContent = `${displayTime(video.duration)}`;
 }
+
+// Click to seek within the video
+// function setProgress(e){
+
+// }
 
 // Volume Controls --------------------------- //
 
@@ -56,3 +72,4 @@ playBtn.addEventListener('click', togglePlay);
 video.addEventListener('click', togglePlay);
 video.addEventListener('timeupdate', updateProgress);
 video.addEventListener('canplay', updateProgress);
+// progressRange.addEventListener('click', setProgress);
