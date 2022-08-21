7c8a6036176b653efec25a1f126dbd4048bd211c
Gautam Sharma <Gautamcodez@gmail.com>
2022-08-21 17:00:54 +0530

    Added progress bar dynamic width functionality

--- Video-player/script.js
+++ Video-player/script.js
@@ -29,9 +29,15 @@ function togglePlay(){
     }
 }
 
+// On video end show play button icon
+video.addEventListener('ended', showPlayIcon);
+
 // Progress Bar ---------------------------------- //
 
-
+// Update progress bar as video plays
+function updateProgress(){
+    progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`;
+}
 
 // Volume Controls --------------------------- //
 
@@ -43,9 +49,10 @@ function togglePlay(){
 
 // Fullscreen ------------------------------- //
 
-// On video end show play button icon
-video.addEventListener('ended', showPlayIcon);
+
 
 // Event Listners
 playBtn.addEventListener('click', togglePlay);
 video.addEventListener('click', togglePlay);
+video.addEventListener('timeupdate', updateProgress);
+video.addEventListener('canplay', updateProgress);
